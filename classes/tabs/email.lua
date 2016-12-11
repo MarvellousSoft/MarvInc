c07d2b4f03b7ebad2d6e2bbc49a7cb14b3f952f1
rilifon <pudimdefrutas@gmail.com>
2016-12-11 19:39:46 -0200

    Scroll with mouse working for emails

--- classes/tabs/email.lua
+++ classes/tabs/email.lua
@@ -19,6 +19,17 @@ EmailTab = Class{
 
         self.main_color = Color.new(150, 30, 240) -- Color of box behind
 
+        -- Stencil function for scrolling
+        self.stencil = function() love.graphics.rectangle("fill", self.pos.x, self.pos.y, self.w, self.h) end
+
+        -- Diference from the original vertical value position for first email (for scrolling)
+        self.dy = 0
+
+        -- Current number of emails
+        self.email_cur = 0
+        -- Maximum number of emails you can have on a screen
+        self.email_on_screen = 16
+
         self.email_height = 35
         self.email_border = 5
 
@@ -27,9 +38,12 @@ EmailTab = Class{
 
     -- Delete an email object mail from the email list
     deleteEmail = function(mail)
-        local mail_list
+        local mail_list, tab
 
-        mail_list = Util.findId("email_tab").email_list
+        tab = Util.findId("email_tab")
+
+        mail_list = tab.email_list
+
         -- Shifts number of all emails greater than the mail to be deleted
         for i, m in ipairs(mail_list) do
             if m.number > mail.number then m.number = m.number - 1 end
@@ -42,6 +56,8 @@ EmailTab = Class{
                 Util.clearTimerTable(mail.handles, MAIN_TIMER)
                 -- Remove from email list
                 table.remove(mail_list, mail.number)
+                -- Update current number of emails
+                tab.email_cur = tab.email_cur - 1
             end
         )
 
@@ -58,6 +74,11 @@ function EmailTab:draw()
 
     love.graphics.rectangle("fill", t.pos.x, t.pos.y, t.w, t.h)
 
+    -- Set stencil for the rectangle containing the code
+    love.graphics.stencil(t.stencil, "replace", 1)
+    -- Only allow rendering on pixels which have a stencil value greater than 0.
+    love.graphics.setStencilTest("greater", 0)
+
     -- Draws email list
     for i,e in ipairs(t.email_list) do
 
@@ -71,14 +92,14 @@ function EmailTab:draw()
         color.a = e.alpha
 
         Color.set(color)
-        love.graphics.rectangle("fill", t.pos.x + t.email_border, t.pos.y + t.email_border*i+ t.email_height*(i-1) + e.juicy_bump, t.w-2*t.email_border, t.email_height)
+        love.graphics.rectangle("fill", t.pos.x + t.email_border, t.pos.y - t.dy*(t.email_height + t.email_border) + t.email_border*i+ t.email_height*(i-1) + e.juicy_bump, t.w-2*t.email_border, t.email_height)
 
         -- Timestamp on the email
         Color.set(Color.new(0, 80, 10,e.alpha))
         font = FONTS.fira(12)
         font_w = font:getWidth(e.time)
         love.graphics.setFont(font)
-        love.graphics.print(e.time,  t.pos.x + t.w - t.email_border - font_w - 5, t.pos.y + t.email_border*i+ t.email_height*(i-1)  + e.juicy_bump)
+        love.graphics.print(e.time,  t.pos.x + t.w - t.email_border - font_w - 5, t.pos.y - t.dy*(t.email_height + t.email_border) + t.email_border*i+ t.email_height*(i-1)  + e.juicy_bump)
 
         -- Author
         font = FONTS.fira(16)
@@ -90,7 +111,7 @@ function EmailTab:draw()
         size = font:getWidth(text)
         font_h = font:getHeight(text)
         love.graphics.setFont(font)
-        love.graphics.print(text,  t.pos.x + t.email_border + 5, t.pos.y + (t.email_height/2 - font_h/2) + t.email_border*i+ t.email_height*(i-1)  + e.juicy_bump)
+        love.graphics.print(text,  t.pos.x + t.email_border + 5, t.pos.y - t.dy*(t.email_height + t.email_border) + (t.email_height/2 - font_h/2) + t.email_border*i+ t.email_height*(i-1)  + e.juicy_bump)
 
         -- Title
 
@@ -104,14 +125,17 @@ function EmailTab:draw()
         font_w = size + font:getWidth(text)
         font_h = font:getHeight(text)
         love.graphics.setFont(font)
-        love.graphics.print(text,  t.pos.x + t.email_border + size, t.pos.y + (t.email_height/2 - font_h/2) + t.email_border*i+ t.email_height*(i-1) + 2 + e.juicy_bump)
+        love.graphics.print(text,  t.pos.x + t.email_border + size, t.pos.y - t.dy*(t.email_height + t.email_border) + (t.email_height/2 - font_h/2) + t.email_border*i+ t.email_height*(i-1) + 2 + e.juicy_bump)
 
         if not e.was_read then
             love.graphics.setFont(FONTS.fira(15))
             Color.set(Color.new(240, 180, 120, e.alpha))
-            love.graphics.print("new",  t.pos.x + t.email_border + 8, t.pos.y - (t.email_height/2 - font_h/2) + t.email_border*i+ t.email_height*(i-1)  + e.juicy_bump)
+            love.graphics.print("new",  t.pos.x + t.email_border + 8, t.pos.y - t.dy*(t.email_height + t.email_border) - (t.email_height/2 - font_h/2) + t.email_border*i+ t.email_height*(i-1)  + e.juicy_bump)
         end
     end
+
+    -- Remove stencil
+    love.graphics.setStencilTest()
 
 end
 
@@ -123,7 +147,7 @@ function EmailTab:mousePressed(x, y, but)
     if not e.email_opened and but == 1 then
         --Check mouse colision with emails
         for i, mail in ipairs(e.email_list) do
-            if mail.alpha > 250 and Util.pointInRect(x,y,{pos = {x = e.pos.x + e.email_border, y = e.pos.y + e.email_border*i+ e.email_height*(i-1)}, w = e.w-2*e.email_border, h = e.email_height}) then
+            if mail.alpha > 250 and Util.pointInRect(x,y,{pos = {x = e.pos.x + e.email_border, y = e.pos.y - e.dy*(e.email_height + e.email_border) + e.email_border*i+ e.email_height*(i-1)}, w = e.w-2*e.email_border, h = e.email_height}) then
                 if not mail.was_read then
                     mail.was_read = true
                     UNREAD_EMAILS = UNREAD_EMAILS - 1
@@ -135,6 +159,23 @@ function EmailTab:mousePressed(x, y, but)
     else
         Opened.mousePressed(x, y, but)
     end
+end
+
+function EmailTab:mouseScroll(x, y)
+    local mx, my
+
+    -- Just enable scroll if there is too much mail in the inbox and mouse is over the inbox screen
+    if self.email_cur - self.email_on_screen <= 0 then return end
+    mx, my = love.mouse.getPosition()
+
+    if Util.pointInRect(mx, my, self) then
+        self.dy = self.dy - y
+    else
+        return
+    end
+
+    self.dy = math.max(self.dy, 0)
+    self.dy = math.min(self.dy, self.email_cur- self.email_on_screen)
 end
 
 -- EMAIL OBJECT --
@@ -175,14 +216,19 @@ EmailObject = Class{
 
 -- Creates a new email and add to the email list
 function email_funcs.new(title, text, author, can_be_deleted, number)
-    local e, mail_list, number
+    local e, mail_list, number, tab
 
-    mail_list = Util.findId("email_tab").email_list
+    tab = Util.findId("email_tab")
 
-    number = Util.tableLen(mail_list) + 1
+    mail_list = tab.email_list
+
+    -- Increase number of current emails and update number for new email
+    tab.email_cur = tab.email_cur + 1
+    number = tab.email_cur
 
     e = EmailObject(title, text, author, can_be_deleted, number)
 
+    -- Add fade-in effect to email
     e.handles["fadein"] = MAIN_TIMER.tween(.5, e, {alpha = 255, juicy_bump = 0}, 'out-quad')
 
     table.insert(mail_list, e)
