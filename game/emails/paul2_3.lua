4b3bd31bcbeb1724047afe0a729332bb4e622ae1
Renato Lui Geh <renatogeh@gmail.com>
2017-02-28 18:00:34 -0300

    Paul now sends an email on the voting results (closes #88).

---
+++ game/emails/paul2_3.lua
@@ -0,0 +1,15 @@
+return {
+    title = "The system, man",
+    text = [[
+First of all, appreciate the help man. Without you we would still be counting the votes. But aww man, shit. I know this is like, democracy, man. But shit. It was a tight vote, but it doesn't make any sense, you know? I mean, the guys at Biochem are totally pro-blunt, but the count says they don't want no legalization. That don't make no sense, right?! Shit man. You think maybe the system is broken? You think maybe this voting system ain't right? Like, instead of dividing by department and making voting optional, we should have mandatory voting for important subjects and an open, direct voting system so gerrymandering, lobbying and the preservation of the status-quo don't dictate the choice of the people? Ya know? But maybe I'm just a dreamer, man. The system ain't right, dude. It ain't.
+
+Fuck the system.
+
+Paul Verkeufen
+Chief engineer at Marvellous Inc's Material Engineering Department
+
+~ LEGALIZE ~
+]],
+    author = "Paul Verkeufen (hempman@med.marv.com)",
+    can_be_deleted = true
+}
--- game/lore_events/paul2.lua
+++ game/lore_events/paul2.lua
@@ -10,4 +10,8 @@ function paul.run()
     LoreManager.timer:after(10, function() Mail.new('paul2_2') end)
 end
 
+function paul.after()
+    Mail.new('paul2_3')
+end
+
 return paul
