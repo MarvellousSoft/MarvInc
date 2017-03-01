6720442ae55253d9b67199a591361a1ebf3029bf
Renato Lui Geh <renatogeh@gmail.com>
2017-03-01 13:16:48 -0300

    Vega's intro mail.

--- game/emails/diego1.lua
+++ game/emails/diego1.lua
@@ -1,9 +1,13 @@
 return {
-    title = "HELLO BRO!",
+    title = "sup bro",
     text = [[
+hey bro whats up. member me? we sat next to each other when we were doing the interview. we made it!
 
+shit man, im hyped. working here at marvinc who would of thought! my mama would be proud. this is so cool man. this place is so huge and there so many stuff to do
 
-diego "the bro" vega
+anyways, just letting u know ill be working with u! were code bros! o man, im pumped yo. this shit is gunna be dope son
+
+Diego Bro-renzo Vega
 ]],
-    author = "Diego Vega (vega@rtd.marv.com)",
+    author = "Diego Lorenzo Vega (vega@rtd.marv.com)"
 }
