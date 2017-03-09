5e03669ffcf3cccc50114be1a4337d67cf183b80
Renato Lui Geh <renatogeh@gmail.com>
2017-03-09 09:14:41 -0300

    Paul 4 (closes #105).

---
+++ game/emails/paul4.lua
@@ -0,0 +1,19 @@
+return {
+    title = "Reactor core",
+    text = [[
+Awesome work on that hallway dude! Good thing "robots" don't have emotions, right? I mean, I'd have gotten PTSD and been scarred for life after having taken all those bodies out the way.
+
+Anyways, now that the pathway is clear my guys can have the reactor repaired. There is a but, though. The reactor had a complete meltdown and the pathway to the control room is on fire.
+
+Now I know what you must be thinking. I thought it too. But this is not the appropriate time or place to think about doing a communal dab, man. It's time to focus on this issue.
+
+Could you put out the fire on the control room so that we can get this situation sorted, please?
+
+Thanks a lot, my man!
+
+Paul Verkeufen
+Chief engineer at Marvellous Inc's Material Engineering Department
+]],
+    author = "Paul Verkeufen (hempman@med.marv.com)",
+    puzzle_id = 'lava'
+}
