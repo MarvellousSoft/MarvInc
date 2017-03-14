be1793741698d81ac650342ddc7aa964978fd847
Renato Lui Geh <renatogeh@gmail.com>
2017-03-13 22:51:16 -0300

    Added email contents for #98.

--- game/emails/diego6.lua
+++ game/emails/diego6.lua
@@ -1,7 +1,7 @@
 return {
     title = "i smeel sumtin fishy bro",
     text = [[
-
+shit bro i dunno man. i feel like im losing my fuckin mind shit i been hearin some shit man some fucked up shit and like they want me to hear it? i mean i dont know man im going crazy. shit so im downstairs at the cafeteria when i see some real pretentious lookin yuppies like shit they couldve come out of some bret ellis novel they so fucked up. talkin about there business cards and their ermenegeelda zelda glasses and shit. and then they like start talkin about some real shady stuff yo like braggin bout usin some kind of medical facility and how they producin bunchs of 'medical waste' and bout how they got deals with hospitals and shit. they say it like they proud. i tellin you bro this shit aint right
 ]],
     author = "Diego Lorenzo Vega (vega@rtd.marv.com)"
 }
