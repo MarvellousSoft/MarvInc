7f129c7b8896ec685788bdf6c7fe807d51555bc2
Yan Couto <yancouto@gmail.com>
2017-09-01 23:21:18 -0300

    Email for Liv7a,b
    Changelog:
     - Fixing email to mention jen (closes #141)

--- marv/emails/liv7_1.lua
+++ marv/emails/liv7_1.lua
@@ -1,8 +1,14 @@
 return {
     title = "Choice is good",
-    text = [[Hey, ]] .. SaveManager.current_user .. [[!
+    text = [[Hey, Chief Engineer!
 
-I'll go easy on you today, and give you an option here. Two tasks, {red}choose one{end}. I'll get someone else to solve the other.
+I see you've been promoted. Maybe we can start improving some stuff around here {red}:D{end}
+
+I also heard about the Jen-itorial promotion {red}HAHAHAHAHHAHAHAHAHAHAHAH{end} I JUST CAN'T {red}HAHAHAHAHAHAH{end}
+
+I really hope I don't need to send you weekly reports of how I am using the Robots' personell. That was a pain in the ass hahah
+
+I'll go easy on you today, and give you an option here. Two tasks, {red}choose one{end}. I'll get someone else to solve the other. Or maybe you should do it. You're a chief now, you should learn to delegate.
 
 So you have to
 
