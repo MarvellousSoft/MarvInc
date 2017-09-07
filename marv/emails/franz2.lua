fb98c9e0d9d4ed9c5d8561da4845ccd353efa81f
Renato Lui Geh <renatogeh@gmail.com>
2017-09-06 23:51:05 -0300

    Franz 2 email. (closes #147)

---
+++ marv/emails/franz2.lua
@@ -0,0 +1,20 @@
+local you = "Employee #" .. EMPLOYEE_NUMBER
+return {
+    title = "[CLASSIFIED] Network issues",
+    text = you .. [[,
+
+As you know, the FBI are investigating MarvInc for ethical misconduct and other accusations that won't hold. Preposterous charges. They have come to our facilities and searched for evidence, but found nothing, thanks to your and Ms. Leubwitz's work. They left empty handed. But are insistent on their crusade against our rightful cause, and refuse to close this case.
+
+I did not expect them, however, to try and exploit network vulnerabilities in our servers. Yes, they have hacked us. But not to worry, they have not yet found any classified information.
+
+Additionally, none of this will matter in the grand scheme of things. Sacrifices are required for the greater good. As Mr. Vega discovered himself. And the FBI will be dealt with... in due time.
+
+But for now you must prevent them from accessing restricted information. Deal with the breach.
+
+Karl Franz F. L. von Linz
+General Executive Manager
+]],
+    author = "Karl Franz F. L. von Linz (kflinz@marv.com)",
+    puzzle_id = 'franz2'
+}
+
