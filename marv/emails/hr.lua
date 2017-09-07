498cd0fd1379649d2c1a89ccb4778570f583ad66
Renato Lui Geh <renatogeh@gmail.com>
2017-09-06 23:51:22 -0300

    Human resources email. (closes #143)

---
+++ marv/emails/hr.lua
@@ -0,0 +1,31 @@
+return {
+    title = "[Notice][HR] A friendly reminder for all of us at MarvInc!",
+    text = [[Hello fellow Marvs!,
+
+We here at Human Resources wish you a joyful day and hope that we are all enjoying our rewarding jobs here at MarvInc!
+
+We have a quick but very important reminder for all of our wonderful employees here at Marv!
+
+When we all signed our contracts to become a member of the Marvellous Family, and started this thrilling adventure that is the Marvellous Career, we all signed a very important document called {blue}The NDA{end}.
+
+{red}You:{end} But what is {blue}The NDA{end}?
+{blue}HR:{end} Well, it is a series of cool and harmless legal obligations that you are now obligated to contractually obey!
+
+{red}You:{end} This sounds scary! Why is it all written in legalese mumbojumbo?!
+{blue}HR:{end} What's written is not really that important! And we definitely did not write in legalese just to confuse you! That's just not the MarvInc way!
+
+{red}You:{end} What are some of these obligations?
+{blue}HR:{end} Well, now this is VERY important! So pay attention now. You are obligated to do the following:
+
+1. The employee gives complete ownership of any and all intelectual property (IP) of his/her during his/her employment at MarvInc to MarvInc.
+2. The employee is forbidden of disclosing information of any and all activities, events, IP or anything related to the employement of MarvInc to anyone who is not working at the same or above capacity as him/her, subject to legal prosecution otherwise.
+3. The employee relinquishes any and all privacy with regards to his/her corporate e-mail, and agrees to the storage of all of his/her e-mails by MarvInc.
+
+These are just the obligations that you should be aware of for now! And remember:
+
+{red}Say no to corporate espionage!{end}
+
+A big hug from all of us at Human Resources!
+]],
+    author = "Human Resources (no_reply@hr.marv.com)"
+}
---
+++ marv/lore_events/nlhr.lua
@@ -0,0 +1,25 @@
+local Mail = require "classes.tabs.email"
+
+local this = {}
+
+this.require_puzzles = {'liv7a', 'liv7b'}
+this.wait = 10
+
+function this.run()
+    Mail.new('hr')
+    LoreManager.timer:after(10, function()
+        Mail.new('nl')
+    end)
+    LoreManager.timer:after(15, function()
+        Mail.new('franz1_3')
+    end)
+    LoreManager.timer:after(25, function()
+        Mail.new('jen6')
+    end)
+    LoreManager.timer:after(17, function()
+        Mail.new('bm3')
+    end)
+    LoreManager.timer:after(19, function()
+        Mail.new('bm3_1')
+    end)
+end
