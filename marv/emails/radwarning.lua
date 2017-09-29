512641de4792bccad7854fb5c35e8adc1ab5378d
rilifon <pudimdefrutas@gmail.com>
2017-09-29 19:21:16 -0300

    Colored binary explanation and radiation-warning emails

--- marv/emails/radwarning.lua
+++ marv/emails/radwarning.lua
@@ -1,22 +1,25 @@
 return {
     title = "WARNING -- HIGH RADIATION LEVELS DETECTED -- WARNING",
     text  = [[
-WARNING -- WARNING -- WARNING -- WARNING -- WARNING
+{orange}-------------------------------------------------------------------------------------------------{end}
+{red}WARNING{orange} -- {red}WARNING{orange} -- {red}WARNING{orange} -- {red}WARNING{orange} -- {red}WARNING{end}
+{orange}-------------------------------------------------------------------------------------------------{end}
+High radiation levels detected at {blue}[WING B]{end}.
 
-High radiation levels detected at [WING B].
-
-All personnel from [WING B], report immediately to your nearest extraction zone. Please follow the red dotted lines located on all corridors, moving in the direction of the red arrows.
+All personnel from {blue}[WING B]{end}, report immediately to your nearest extraction zone. Please follow the {red}red dotted lines{end} located on all corridors, moving in the direction of the {red}red arrows{end}.
 
 Once in the extraction zone, please report to the nearest:
-  1. [RSO] for [RADIATION SCAN AND DECONTAMINATION].
-  2. [EMT] for [MEDICAL].
-  3. [MARV INC LAWYER] for [SETTLEMENT].
+  1. {orange}[RSO]{end} for {orange}[RADIATION SCAN AND DECONTAMINATION]{end}.
+  2. {purple}[EMT]{end} for {purple}[MEDICAL]{end}.
+  3. {green}[MARV INC LAWYER]{end} for {green}[SETTLEMENT]{end}.
 
-Refer to your MarvInc handbook for section [N-25] for more information on how to take the appropriate safety measures.
+Refer to your MarvInc handbook for {blue}Section [N-25]{end} for more information on how to take the appropriate safety measures.
 
-This is an automated emergency service e-mail. Do not reply.
+{gray}This is an automated emergency service e-mail. Do not reply{end}.
+{orange}-------------------------------------------------------------------------------------------------{end}
+{red}WARNING{orange} -- {red}WARNING{orange} -- {red}WARNING{orange} -- {red}WARNING{orange} -- {red}WARNING{end}
+{orange}-------------------------------------------------------------------------------------------------{end}
 
-WARNING -- WARNING -- WARNING -- WARNING -- WARNING
 ]],
     author = "Automated Emergency Service (no-reply@aes.marv.com)",
     can_be_deleted = true
