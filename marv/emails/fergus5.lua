ab08bc3861cdee7f83a2397bb1e199dc5b4d7812
Yan Couto <yancouto@gmail.com>
2017-10-10 18:52:10 -0300

    Fergus 5
    Changelog:
    - Fergus email 5, still unlinked (closes #148)

---
+++ marv/emails/fergus5.lua
@@ -0,0 +1,28 @@
+return {
+    title = "READ THIS " .. string.upper(SaveManager.current_user),
+    text = SaveManager.current_user .. [[!!!
+
+This is Fergus. I {red}finally{end} got hold of you. I wasn't sure you were gonna figure out that spam email wasn't spam, but you'll understand why I {red}couldn't{end} contact you directly.
+
+{orange}Sit tight{end}, this will be a long read.
+
+After I was fired, I went home and cried like a girl, {orange}I won't deny{end}. All my bosses had treated me like shit for the past week, especially that {red}bitch{end} Olivia. Jen also took her frustation out on me, and Paul didn't lift a finger when Franz {blue}cold-heartedly{end} told me I had no place in the company.
+
+The next days were rough, but I managed to get myself together. I always knew there was something wrong with Marvellous Inc. {red}Always{end}. Even when I was being an asshole to the interns, but at that time I just ignored it. Weird tasks, a lot of confidential stuff, a lot of promises, and very little paperwork ({orange}who would've thought{end}).
+
+I tried hacking Marvellous, {orange}unsuccessfully of course{end}. But it wasn't in vain. The feds found me, and I was able to help them. My insider knowledge was very helpful to them, thankfully, and they were able to find out a lot of Marvellous Inc's secrets. Once they figured out I was the mole, they came after me. Jesus, {orange}it makes me scared to even write it{end}. I couldn't sleep for days. But the FBI helped me fake my death, I changed my name, and here I am now. I still have trouble sleeping, for the fear of getting caught, and for everything I know...
+
+There's no easy way to break these news for you, ]] .. SaveManager.current_user .. [[... {red}The robots are people.{end} They are developing some sort of mind control component, and we have been helping them! The FBI still don't know where they are getting the people from, but the rate of disappearances hasn't increased lately... Which leads to the frightening conclusion that they have been doing this for a long time.
+
+Well, there it is. {orange}The whole truth{end}. Now comes your part, ]] .. SaveManager.current_user .. [[. {red}You must help me{end}. Me and the FBI. They have a solid case... But Marvellous is too big. They won't go down easy, we need more proof... Something from inside.
+
+I can't help you from outside but I'm sure you'll figure it out, ]] .. SaveManager.current_user .. [[. You're the real hero here. {red}This is your story{end}. You can bring the company down. Don't you worry about anything you might need to do, the FBI can get us immunity from anything done while employees of Marvellous Inc.
+
+Please do the right thing,
+
+Your truest friend,
+Fergus
+]],
+    author = "Jorge Davidson (jorge.davidson.74837897@gmail.com)",
+    can_be_deleted = true
+}
