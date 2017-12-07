20b58f8a9a01ce5273a5df870d7d01ca2387c6b8
Renato Lui Geh <renatogeh@gmail.com>
2017-12-07 21:17:06 -0200

    Added Bill and Miles' emails for Bill 3. (#144)

---
+++ marv/emails/bm3_1.lua
@@ -0,0 +1,18 @@
+return {
+    title = "RE: ",
+    text = [[
+{cyan}So what{end} is all this convolution, Billy boy?
+
+Ain't no reason to be {green}blue{end}.
+
+These corporate cats, their {blue}kind{end} dont know what math is. They just lookin to get more bread and ain't thinkin bout the jam, right?
+
+What I mean is, its they just {red}Freddie Freeloaders{end}, alright? Yall get {orange}all blues{end} on account of this shit, but aint no shit ya can do.
+
+Ain't no reason to comply neither, though. I got some of that {gray}Flamenco{end} kind of {gray}sketch{end}, if ya dig me Billy boy. So aint no reason to worry. We all good.
+
+So just do what you gots to do, cats. Uncle Miles got all figured out.
+]],
+    author = "Miles Evans (miles@mat.marv.com)",
+    can_be_deleted = true
+}
