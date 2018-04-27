9bee67332d0a5205a153d6e50c8a4ea7409b39e5
Yan Couto <yancouto@gmail.com>
2018-04-27 11:08:11 -0300

    Lore event for Diego1_2

---
+++ marv/lore_events/diego1_2.lua
@@ -0,0 +1,21 @@
+--[[
+#####################################
+Marvellous Inc.
+Copyright (C) 2017  MarvellousSoft & USPGameDev
+See full license in file LICENSE.txt
+(https://github.com/MarvellousSoft/MarvInc/blob/dev/LICENSE.txt)
+#####################################
+]]--
+
+local Mail = require "classes.tabs.email"
+
+local diego = {}
+
+diego.require_puzzles = { 'fergus1', 'jen1' }
+diego.wait = 5 + 60 * 5
+
+function diego.run()
+    Mail.new('diego1_2')
+end
+
+return diego
