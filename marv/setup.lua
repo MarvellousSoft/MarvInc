3550d0f8bc4fb846bc8c55f5c08253f0e8d11d5f
Rilifon <pudimdefrutas@gmail.com>
2018-06-26 20:42:15 -0300

    Bump to ver 1.3.0

--- marv/setup.lua
+++ marv/setup.lua
@@ -15,7 +15,7 @@ local setup = {}
 --SETUP FUNCTIONS
 --------------------
 
-VERSION = "1.2.2"
+VERSION = "1.3.0"
 
 --GLOBAL VARIABLES--
 W = love.graphics.getWidth() --Current width of the game window
