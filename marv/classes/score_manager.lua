cc4edc7d04f0479b436c43170e7f842b264d1f6e
rilifon <pudimdefrutas@gmail.com>
2018-12-17 16:54:16 -0200

    Leaderboards have an error and loading state

--- marv/classes/score_manager.lua
+++ marv/classes/score_manager.lua
@@ -17,16 +17,15 @@ function sm.uploadCompletedStats(puzzle)
     if not pop then return end
     pop:translate(-260,0)
     Steam.userStats.findOrCreateLeaderboard(id .. '_linecount', "Ascending", "Numeric", function (info, err)
-        if err or info == nil then lb:error(); return end
+        if err or info == nil then lb:gotError(); return end
         Steam.userStats.uploadLeaderboardScore(info.steamLeaderboard, "KeepBest", line_count, nil, function(_, err2)
-            if err2 then lb:error(); return end
+            if err2 then lb:gotError(); return end
             print('Stats uploaded to leaderboard: completed ' .. id .. ' with ' .. line_count .. ' lines')
             Steam.userStats.downloadLeaderboardEntries(info.steamLeaderboard, "Global", 1, 10000, function(results, err3)
-                if err3 or results == nil then lb:error(); return end
+                if err3 or results == nil then lb:gotError(); return end
                 for i, r in ipairs(results) do
                     results[i] = r.score
                 end
-                print(line_count)
                 lb:showResults(results, line_count)
             end)
         end)
