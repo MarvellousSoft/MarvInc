d420c8bff02b04631b4a51a2afba4366fb1d9923
Yan Couto <yancouto@gmail.com>
2019-01-30 18:34:25 -0200

    First level custom copy

---
+++ marv/lparser/examples/tuto1/level.lua
@@ -0,0 +1,60 @@
+Meta.SetName "Hello World"
+Meta.SetID "1"
+Meta.SetLines(99)
+Meta.SetMemory(0)
+
+Objective.SetText "Just get to the red tile. It's not that hard."
+
+Objective.SetCheck(function(grid)
+    -- local i, j = Bot.GetPosition()
+    -- return i == 1 and j == 21
+    -- This also works
+    return grid[1][21] and grid[1][21].type == 'bot'
+end)
+
+Floor.Register('red_tile', 'r')
+Floor.PlaceAt('r', 1, 21)
+
+local w = Wall {}
+Objects.Register(w, 'o')
+-- The Object Layer (string).
+Objects.SetAll("ooooooooo------------"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo-ooooooooooo"..
+               "ooooooooo------------")
+
+Bot.SetPosition(21, 21)
+Bot.SetOrientation "west"
+
+Game.SetOnEnd(function()
+    return {
+        title = "Congrats",
+        text  = "You finished the custom copy of the first level.",
+        color = "blue",
+        button1 = {
+            text = "k tks",
+            color = "red",
+        },
+        button2 = {
+            text = "fuck u",
+            color = "black"
+        }
+    }
+end)
