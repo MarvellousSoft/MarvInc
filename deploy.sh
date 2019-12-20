0b81e548949c37af77b23c5e7f57f0c4e333fae7
Yan Soares Couto <yancouto@fb.com>
2019-12-20 00:30:22 -0300

    Adapting deploy.sh to newest SDK and love-release versions

--- deploy.sh
+++ deploy.sh
@@ -151,30 +151,30 @@ function post_build_platform {
     rm ./build/Marvellous_Inc-$WIN.zip
     cp $TMP_PATH/win/Marvellous_Inc-$WIN.zip ./build/
   elif [ "${_plt}" == "M" ]; then
-    if [ "$STEAM" -ne $NULL ]; then
-      cp ./sdk/redistributable_bin/osx32/libsteam_api.dylib $TMP_PATH/mac/
-    fi
     printf "Adding custom icon to MAC OS X build.\n"
     pushd .
     mkdir -p $TMP_PATH/mac
-    cp ./build/Marvellous_Inc-macosx.zip $TMP_PATH/mac/
+    if [ "$STEAM" -ne $NULL ]; then
+      cp ./sdk/redistributable_bin/osx/libsteam_api.dylib $TMP_PATH/mac/
+    fi
+    cp ./build/Marvellous_Inc-macos.zip $TMP_PATH/mac/
     cp ./Marvellous_Inc.icns $TMP_PATH/mac/
     cd $TMP_PATH/mac
     rm -rf ./Marvellous_Inc.app
-    unzip Marvellous_Inc-macosx.zip
+    unzip Marvellous_Inc-macos.zip
     cp ./Marvellous_Inc.icns ./Marvellous_Inc.app/Contents/Resources/GameIcon.icns
     cp ./Marvellous_Inc.icns ./Marvellous_Inc.app/Contents/Resources/OS\ X\ AppIcon.icns
-    rm Marvellous_Inc-macosx.zip
+    rm Marvellous_Inc-macos.zip
     if [ "$STEAM" -ne $NULL ]; then
       download_luasteam "osx" "luasteam.so"
       printf "Copying luasteam.so and libsteam_apy.dylib with MacOS App...\n"
-      zip Marvellous_Inc-macosx.zip -r ./Marvellous_Inc.app libsteam_api.dylib luasteam.so
+      zip Marvellous_Inc-macos.zip -r ./Marvellous_Inc.app libsteam_api.dylib luasteam.so
     else
-      zip Marvellous_Inc-macosx.zip -r ./Marvellous_Inc.app
+      zip Marvellous_Inc-macos.zip -r ./Marvellous_Inc.app
     fi
     popd
-    rm ./build/Marvellous_Inc-macosx.zip
-    cp $TMP_PATH/mac/Marvellous_Inc-macosx.zip ./build/
+    rm ./build/Marvellous_Inc-macos.zip
+    cp $TMP_PATH/mac/Marvellous_Inc-macos.zip ./build/
   fi
 
   printf "Finished post-build.\n"
