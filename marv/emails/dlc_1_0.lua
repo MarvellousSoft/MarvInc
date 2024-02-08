0cfd5d8440b8e1facae75490a9f13c5fbcb830d3
Ricardo Lira <pudimdefrutas@gmail.com>
2024-02-08 17:35:51 -0300

    Dlc 1.0 email

--- marv/emails/dlc_1_0.lua
+++ marv/emails/dlc_1_0.lua
@@ -8,10 +8,22 @@ See full license in file LICENSE.txt
 ]]--
 
 return {
-    title = "DLC 1.0 placeholder title",
+    title = "Exciting Opportunity for Collaboration",
     text = [[
-DLC 1.0 placeholder text
+Dear, employee #]] .. EMPLOYEE_NUMBER .. [[,
+
+I trust this message finds you in great spirits and enjoying the dynamic challenges that come with our innovative work at Marvellous Inc. I've been following your recent accomplishments closely, and I must say, your {yellow}exceptional{end} problem-solving skills have reached the minds and hearts within our {cyan}HI{end} department.
+(That is {cyan}Human Intelligence{end}, since we don't like to use the word {gray}Artificial{end} around here {green};){end})
+
+In the spirit of fostering collaboration and leveraging the extraordinary talents within our team, I find myself faced with a {purple}unique opportunity{end} that aligns perfectly with your capabilities. Recently, I had the privilege of addressing a rather {purple}complex scenario{end} in a certain sector of our division, a sort of {gray}"emergency shift"{end} within the hierarchical structure between our fellow employees, and where a {purple}meticulous touch{end} is required to ensure optimal functionality and efficiency moving on.
+
+Since it comes to my attention that this is one of your {yellow}specialties{end}, I've attached the problem within this email. Please, whenever you have the time, we would love your input so we can resume our activities as soon as possible.
+
+Best regards and carry on,
+
+Karen Bakshi
+Lead Manager at Marvellous Inc. HI Deparment
 ]],
-    author = "Karen Bakshi (karen.b@aid.marv.com)",
+    author = "Karen Bakshi (karen.b@hid.marv.com)",
     puzzle_id = 'dlc_1_b1'
 }
