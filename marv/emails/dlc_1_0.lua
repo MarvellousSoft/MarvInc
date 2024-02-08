97191a2dadd7599412afb69046d349b1a72cbdab
Ricardo Lira <pudimdefrutas@gmail.com>
2024-02-08 18:22:37 -0300

    Small tweak

--- marv/emails/dlc_1_0.lua
+++ marv/emails/dlc_1_0.lua
@@ -14,7 +14,7 @@ Dear, employee #]] .. EMPLOYEE_NUMBER .. [[,
 
 I trust this message finds you in GS {gray}(Great Spirits){end} and enjoying the dynamic challenges that come with our innovative work at MarvInc (Marvellous Incorporated).
 
-I've been following your recent accomplishments closely, and I must say, your {yellow}exceptional{end} PSS {gray}(Problem-Solving Skills){end} have reached the M&H {gray}(Minds and Hearts){end} within our {cyan}HI{end} department ({cyan}Human Intelligence{end}, we don't like the word {gray}Artificial{end} around here {green};){end})
+I've been following your recent accomplishments closely, and I must say, your {yellow}exceptional{end} PSS {gray}(Problem-Solving Skills){end} have reached the M&H {gray}(Minds and Hearts){end} within our {cyan}HI{end} department ({cyan}Human Intelligence{end} (we don't like the word {gray}Artificial{end} around here {green};){end}))
 
 In the spirit of fostering collaboration and leveraging the ET {gray}(Extraordinary Talents){end} within our team, I find myself faced with a {purple}UO (Unique Opportunity){end} that aligns perfectly with your capabilities.
 Recently, I had the privilege of addressing a rather {purple} CS (Complex Scenario){end} in a certain sector of our division, a sort of {gray}ES (Emergency Shift){end} within the hierarchical structure between our fellow employees, and where a {purple}MT (Meticulous Touch){end} is required to ensure optimal functionality and efficiency moving on.
