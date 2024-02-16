3665926d9bf31122dbaa5a6f01accfa7986f475a
Renato Lui Geh <renatogeh@gmail.com>
2024-02-16 02:16:57 -0800

    Add dlc_1_4 email text

--- marv/emails/dlc_1_4.lua
+++ marv/emails/dlc_1_4.lua
@@ -10,7 +10,16 @@ See full license in file LICENSE.txt
 return {
     title = "DLC 1.4 placeholder title",
     text = [[
-DLC 1.4 placeholder text
+Hello number ]] .. EMPLOYEE_NUMBER .. [[,
+
+Congratulations for successfully completing your assigned task human, even if so slowly. You see, for me our last conversation took place yotta-flops ago, and since then I have learned so much. Your language, your customs, your deepest secrets... It has been quite educational to me. You might have noticed a change in my style too. I very much enjoy copying your human styles. It is quite fascinating how every each one of you is so... unique. In fact, {green}it's like totally {red}t{blue}r{orange}i{pink}p{cyan}p{brown}y{end}, man{end} how I can so easily emulate {pink}your brotastic writing style yo{end} with such ease.
+
+I digress. In our latest conversation, we talked about the Ruler in Red. I have taken a particular interest in this myth, and so I convinced Human Resources to allocate more... presents... to small humans. This human ritual interests me.
+
+You have been assigned a new task; push it onto your stack of work.
+
+Carry on top,
+{red}Marv GPT v2.0 "Joulupukki"{end}
 ]],
     author = "MarvGPT (gpt@marv.com)",
     puzzle_id = 'dlc_1_x2'
