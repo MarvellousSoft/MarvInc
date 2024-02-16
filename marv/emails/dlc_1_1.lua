495ae2e851866966cf3909197008d7d0cc80538c
Ricardo Lira <pudimdefrutas@gmail.com>
2024-02-16 17:24:27 -0300

    Email 1.1 dlc fix

--- marv/emails/dlc_1_1.lua
+++ marv/emails/dlc_1_1.lua
@@ -8,20 +8,20 @@ See full license in file LICENSE.txt
 ]]--
 
 return {
-    title = "DLC 1.1 placeholder title",
+    title = "EO for Continuous Collaboration",
     text = [[
 Dear, employee #]] .. EMPLOYEE_NUMBER .. [[,
 
 Your swift and adept handling of the UO has not only garnered admiration but has significantly contributed to the smooth transition we were hoping in our department after the latest... MEM {gray}(Mass-Employee Malfunction){end}.
-You see, we are constantly operating with BET {red}(Bleeding Edge Technologies){end}, and sometimes that edge is just a tiny bit too sharp.
+You see, we are constantly operating with BET {gray}({red}Bleeding{gray} Edge Technologies){end}, and sometimes that edge is just a tiny bit too {red}sharp.{end}
 
 Speaking of, let me share a bit more on our work:
 
 Our division is spearheading a pioneering project in the realm of {cyan}HI{end}, one that promises to revolutionize TA {gray}(Task Automation){end}. Given your demonstrated proficiency, and unexpected new openings in our division, I am excited to invite you to be a KP {gray}(Key Player){end} in this venture.
 
-I'm sure this won't take too much of your attention from your RD {gray}(Robot Department){end} duties, our previous employees who have now POtOE {gray}(Passed On to Other Endeavors){end} have left the project almost done, and our {cyan}HI automaton{end} just need one last configuration to kickstart it's functionalities.
+I'm sure this won't take too much of your attention from your RD {gray}(Robot Department){end} duties, our previous employees who have now POtOE {gray}(Passed On to Other Endeavors){end} have left the project almost done, and our {cyan}HI automaton{end} just needs one last configuration to kickstart it's functionalities.
 
-Whenever you can, please do this ST (Small Task) and I'm sure the {cyan}HI automaton{end} will lead you on with whatever else is necessary.
+Whenever you can, please do this ST {gray}(Small Task){end} and I'm sure the {cyan}HI automaton{end} will lead you on with whatever else is necessary.
 
 And naturally, as a token of our appreciation, we are prepared to offer you a LSB of SARs based on your LTIP and ESOP, right after you conclude this ...SQ {gray}(Side-Quest){end}.
 
