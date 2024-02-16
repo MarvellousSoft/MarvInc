3498ba9a9f1ae30dd133549093774a1313284f26
Ricardo Lira <pudimdefrutas@gmail.com>
2024-02-16 17:18:12 -0300

    Fix 1.0 dlc email

--- marv/emails/dlc_1_0.lua
+++ marv/emails/dlc_1_0.lua
@@ -12,14 +12,14 @@ return {
     text = [[
 Dear employee #]] .. EMPLOYEE_NUMBER .. [[,
 
-I trust this message finds you in GS {gray}(Great Spirits){end} and enjoying the dynamic challenges that come with our innovative work at MarvInc (Marvellous Incorporated).
+I trust this message finds you in GS {gray}(Great Spirits){end} and enjoying the dynamic challenges that come with our innovative work at {green}MarvInc {gray}(Marvellous Incorporated){end}.
 
 I've been following your recent accomplishments closely, and I must say, your {yellow}exceptional{end} PSS {gray}(Problem-Solving Skills){end} have reached the M&H {gray}(Minds and Hearts){end} within our {cyan}HI{end} department ({cyan}Human Intelligence{end} (we don't like the word {gray}Artificial{end} around here {green};){end}))
 
-In the spirit of fostering collaboration and leveraging the ET {gray}(Extraordinary Talents){end} within our team, I find myself faced with a {purple}UO (Unique Opportunity){end} that aligns perfectly with your capabilities.
-Recently, I had the privilege of addressing a rather {purple} CS (Complex Scenario){end} in a certain sector of our division, a sort of {gray}ES (Emergency Shift){end} within the hierarchical structure between our fellow employees, and where a {purple}MT (Meticulous Touch){end} is required to ensure optimal functionality and efficiency moving on.
+In the spirit of fostering collaboration and leveraging the ET {gray}(Extraordinary Talents){end} within our team, I find myself faced with a {purple}UO {gray}(Unique Opportunity){end} that aligns perfectly with your capabilities.
+Recently, I had the privilege of addressing a rather {purple}CS {gray}(Complex Scenario){end} in a certain sector of our division, a sort of {red}ES {gray}(Emergency Shift){end} within the hierarchical structure between our fellow employees, and where a {purple}MT {gray}(Meticulous Touch){end} is required to ensure optimal functionality and efficiency moving on.
 
-Since it comes to my attention that this is one of your {yellow}specialties{end}, I've attached the problem within this email. Please, whenever you have the time, we would love your input so we can RoA {gray}(Resume our Activities){end} as soon as possible.
+Since it comes to my attention that this is one of your {yellow}specialties{end}, I've attached the problem within this email. Please, whenever you have the time, we would love your input so we can RtW {gray}(Return to Work){end} as soon as possible.
 
 Best regards and CO {gray}(Carry On){end},
 
