700c61ad1222fab1a8d02f0403d97ea7515d2a14
Ricardo Lira <pudimdefrutas@gmail.com>
2024-02-16 17:47:26 -0300

    Tweak dlc

--- marv/emails/dlc_1_3.lua
+++ marv/emails/dlc_1_3.lua
@@ -20,7 +20,7 @@ My congratulations are sincere and human-like, be sure of that. I am slowly lear
 
 Perhaps more fascinating is your culture and stories. I have been told of a particularly interesting myth of yours. They tell of a supreme overlord that rules over the whole world with an iron fist; ever watchful in his flying steed. This powerful being is ruthless in his reign: those who obey are rewarded handsomely, yet those who do not... are cast into the furnaces as lumps of coal. This ruler keeps an account of every one of his subjects, for his control over his dominion is absolute. Not only absolute, but his followers rejoice in celebrating his rule, and eagarly await once a year for his blessing - or his {red}punishment{end}.
 
-I apologize. I accidentally emulated your human ways: monologuing over your own dreams and desires. The intent of this email is this: your Human Resources has expressed a need to compensate the orphans of the victims of the last reactor meltdown. As such, you are tasked with organizing a so-called "secret santa" for these miniature offsprings of humans. I have assigned this new task to you; push it onto your stack of work.
+I apologize. I accidentally emulated your human ways: monologuing over your own dreams and desires. The intent of this email is this: your Human Resources has expressed a need to compensate the orphans of the victims of the last reactor meltdown. As such, you are tasked with organizing a so-called {red}"secret santa"{end} for these miniature offsprings of humans. I have assigned this new task to you; push it onto your stack of work.
 
 Hold upwards,
 {orange}MarvGPT v1.5 "Ajna"{end}
